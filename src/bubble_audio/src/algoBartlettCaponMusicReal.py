6c61f2886caa514cd3ea22a8d82e5e788c374bdb
Camille78 <camille.soulie19@gmail.com>
2017-03-01 18:04:05 +0100

    ajout Adaptation algo pour des signaux rééls

---
+++ src/bubble_audio/src/algoBartlettCaponMusicReal.py
@@ -0,0 +1,116 @@
+import wave
+import scipy.io.wavfile as sciwave
+from scipy.fftpack import fft
+import math
+import numpy as np
+import pyaudio
+import pylab
+
+# ========= (2) RECEIVED SIGNAL =========
+
+Fs, a = sciwave.read('90-180pool1.wav')
+a = np.array(a)
+a = a / (2.0**15 - 1.0)
+
+####### Parametres modifiables ###
+
+M = 1;                       # Number of sources
+N = 2;                       # Number of microphones(on doit avoir M <= N - 1)
+dist = .08;                  # Distance between adjacent microphones(en m)
+# c = 346.287;               # Speed of sound in air(en m/s)
+c = 1500;                    # Speed of sound in water
+f = 15000;                   # Signal frequency(en Hz)
+
+lfft = 1024 * 1;             # Number of data points for FFT in a snapshot
+K = math.floor(np.size(a[:,0]) / lfft); # Number of frequency snapshots(multiple of lfft)
+L = int(K * lfft);                # Number of data snapshots recorded by receiver
+y = a[0:L,:];                # Truncated signal(contient nbre entier de snapshots)
+
+T = 1 / Fs;
+rx=np.linspace(-(N-1)/2.0,(N-1)/2.0,N).reshape(N,1); # Assume uniform linear array (on peut avoir elevation si on fait array rectangulaire avec min 3 hydrophones)
+ry=np.zeros((N,1),float);
+rz=np.zeros((N,1),float);
+r = np.concatenate((rx,ry,rz),axis=1); # Assume uniform linear array
+
+## Forme complexe du signal recu
+df = float(Fs)/float(lfft)/1.0;                 # frequency grid size
+F = np.arange(0,Fs/1,df);
+X=np.zeros((int(lfft),int(N),int(K)),dtype='complex');
+for ih in range (0,int(N)):
+    for iv in range (0,int(K)):
+        pos=iv*lfft;
+        tmp=y[pos:pos+lfft,ih];
+        X[:,ih,iv]=fft(tmp);
+
+
+# Find the closest frequency from the discrete frequency vector resulted from FFT
+values=abs(F-f);
+mf=np.amin(values);
+mi=np.argmin(values)
+f0=F[mi];
+
+x0=np.zeros((int(N), int(K)),dtype='complex');
+for ih in range (0,int(N)):
+    for iv in range (0,int(K)):
+        x0[ih,iv]=X[mi,ih,iv]; # signal complexe
+
+## Common parameters for all algo
+
+# Sample covariance matrix
+Rxx = np.dot(x0,x0.conj().T)/L;
+print(Rxx)
+# Search directions
+AzSearch = np.linspace(0,180,181); # Azimuth values to search
+ElSearch = np.zeros(AzSearch.shape); # Simple 1D example
+
+
+# Corresponding points on array manifold to search
+kSearch = math.pi*np.array([np.cos(np.radians(AzSearch))*np.cos(np.radians(ElSearch)), np.sin(np.radians(AzSearch))*np.cos(np.radians(ElSearch)), np.sin(np.radians(ElSearch))]);
+ASearch = np.exp(-1j*np.dot(r,kSearch));
+
+###############################################################################################################
+## Bartlett or Conventional Beamforming
+
+#Bartlett spectrum
+Zbf=[]
+for i in range (0,np.size(AzSearch)):
+    Z=(ASearch[:,i].conj().T).dot(Rxx).dot(ASearch[:,i]);
+    Zbf.append(abs(Z));
+
+pylab.figure()
+pylab.plot(AzSearch,10*np.log10(Zbf/max(Zbf)))
+
+#################################################################################################################
+## Capon
+
+# Capon spectrum
+Zc=[]
+for i in range (0,np.size(AzSearch)):
+    Z=(ASearch[:,i].conj().T).dot(np.linalg.inv(Rxx)).dot(ASearch[:,i]);
+    Zc.append(abs(1/Z));
+pylab.plot(AzSearch,10*np.log10(Zc/max(Zc)))
+
+
+##################################################################################################################
+## MUSIC
+
+# Eigendecompose
+D,E = np.linalg.eig(Rxx);
+
+lambd = np.sort(D); # Vector of sorted eigenvalues
+idx = np.argsort(D); # Index of sorted eigenvalues
+
+
+E = E[:,idx]; # Sort eigenvalues accordingly
+En = E[:,0:np.size(idx)-M]; # Noise eigenvectors (ASSUMPTION: M IS KNOWN)
+
+
+
+# MUSIC spectrum
+Zm=[]
+for i in range (0,np.size(AzSearch)):
+    Z = (ASearch[:, i].conj().T).dot(En).dot(En.conj().T).dot(ASearch[:, i]);
+    Zm.append(abs(1/Z));
+
+pylab.plot(AzSearch,10*np.log10(Zm/max(Zm)))
+pylab.show()
